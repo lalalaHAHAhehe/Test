e616b3ba6ee18aec8823c4d0157b24eb5bab5ef2
Nick <chenjiayicjy123@dingtalk.com>
2021-08-04 10:31:34 +0800

    first commit

---
+++ src/test/java/com/meter/CoreTest.java
@@ -0,0 +1,20 @@
+package com.meter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.util.Assert;
+
+@SpringBootTest
+public class CoreTest {
+
+    @Test
+    public void testTrue() {
+        Assert.isTrue(true, "啦啦啦");
+    }
+
+    @Test
+    public void testFail() {
+        Assert.isTrue(false, "错咯");
+    }
+
+}
