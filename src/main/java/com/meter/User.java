67071bccdd8eae8158ec8013a04d48b645006935
Nick <chenjiayicjy123@dingtalk.com>
2021-08-09 15:49:35 +0800

    test

---
+++ src/main/java/com/meter/User.java
@@ -0,0 +1,32 @@
+package com.meter;
+
+public class User {
+
+    private String name;
+    private String ip;
+    private String sex;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getIp() {
+        return ip;
+    }
+
+    public void setIp(String ip) {
+        this.ip = ip;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+}
