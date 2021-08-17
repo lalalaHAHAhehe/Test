b3f599c0848b6a470bc0ecb36017ba681f59f9e5
Nick <chenjiayicjy123@dingtalk.com>
2021-08-17 18:11:52 +0800

    test

--- src/test/java/com/meter/TestNG.java
+++ src/test/java/com/meter/TestNG.java
@@ -8,12 +8,14 @@ public class TestNG {
     @Test
     public void success(){
         System.out.println("JunitTest fail");
+        A.a();
         Assertions.assertTrue(true, "123");
     }
 
     @Test
     public void fail(){
         System.out.println("JunitTest fail");
+        A.a();
         Assertions.assertTrue(false, "123");
     }
 
